109472bb980fd736780e407bb97e18adc976c27b
fion-lei <137603599+fion-lei@users.noreply.github.com>
2024-11-29 18:49:16 -0700

    Updated reserve query

--- revRentals/myApp/reservations_views.py
+++ revRentals/myApp/reservations_views.py
@@ -15,21 +15,21 @@ class AddReservationView(APIView):
             start_date = data.get("start_date")
             end_date = data.get("end_date")
             status = "Pending Approval"
-            print(profile_id)
+
             with connection.cursor() as cursor:
                 cursor.execute("SELECT admin_id FROM admin LIMIT 1")  # Assumes there's only one admin
                 admin_id = cursor.fetchone()[0]  # Fetch the first (and only) admin ID
             
                 cursor.execute("""
                                INSERT INTO reservation(Profile_ID, Admin_ID, Product_no, VIN, Lot_No, Start_Date, End_Date, Status)
-                               VALUES(%s,%s,%s,%s,%s,%s,%s)
+                               VALUES(%s,%s,%s,%s,%s,%s,%s,%s)
                                """, [profile_id, admin_id, product_no, vin, lot_no, start_date, end_date, status])
             
             print("reservation was added")   
-            return Response({"success": True, "message": "Reservation added successfully."}, status=status.HTTP_201_CREATED)
+            return Response({"success": True, "message": "Reservation added successfully."})
         except Exception as e:
             # Handle any errors and return an appropriate response
-            print("Error occurred:", str(e))
+            print("Error occurred trying to add a reservation:", str(e))
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     
