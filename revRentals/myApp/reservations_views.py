4822487d248c8b9f6a45fba7a90fbee52203bf99
fion-lei <137603599+fion-lei@users.noreply.github.com>
2024-11-29 19:20:19 -0700

    Fixed status

--- revRentals/myApp/reservations_views.py
+++ revRentals/myApp/reservations_views.py
@@ -14,19 +14,25 @@ class AddReservationView(APIView):
             vin = data.get("vin")
             start_date = data.get("start_date")
             end_date = data.get("end_date")
-            status = "Pending Approval"
-
+            print("VIN", vin)
+            print("Lot_no", lot_no)
+            print("Product_no", product_no)
             with connection.cursor() as cursor:
                 cursor.execute("SELECT admin_id FROM admin LIMIT 1")  # Assumes there's only one admin
                 admin_id = cursor.fetchone()[0]  # Fetch the first (and only) admin ID
-            
-                cursor.execute("""
-                               INSERT INTO reservation(Profile_ID, Admin_ID, Product_no, VIN, Lot_No, Start_Date, End_Date, Status)
-                               VALUES(%s,%s,%s,%s,%s,%s,%s,%s)
-                               """, [profile_id, admin_id, product_no, vin, lot_no, start_date, end_date, status])
-            
-            print("reservation was added")   
-            return Response({"success": True, "message": "Reservation added successfully."})
+                if ((vin or lot_no or product_no)== None):
+                    return Response({"Error": "Missing one of three parameters (VIN, Lot_No, Product_No)"},status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    cursor.execute("""
+                                INSERT INTO reservation(Profile_ID, Admin_ID, Product_no, VIN, Lot_No, Start_Date, End_Date, Status)
+                                VALUES(%s,%s,%s,%s,%s,%s,%s,%s)
+                                """, [profile_id, admin_id, product_no, vin, lot_no, start_date, end_date, "Pending Approval"])
+                    print("Reservation added.")
+                    return Response(
+                        {"success": True, "message": "Reservation added successfully."},
+                        status=status.HTTP_201_CREATED
+                    )
+
         except Exception as e:
             # Handle any errors and return an appropriate response
             print("Error occurred trying to add a reservation:", str(e))
