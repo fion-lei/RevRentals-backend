55608f9ee14adc2203bbecbe90bfe66bdd2ec39e
Peter <peter.tran2@ucalgary.ca>
2024-12-09 17:36:34 -0700

    backend cleanup

--- revRentals/myApp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class userSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password']
